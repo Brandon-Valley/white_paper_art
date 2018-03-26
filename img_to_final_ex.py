b5869f023e630314a253449c6eb8d6ec4512ff9a
Brandon Valley <bavvh8@mst.edu>
2018-03-26 01:30:17 -0500

    about to go back to check something

--- img_to_final_ex.py
+++ img_to_final_ex.py
@@ -6,6 +6,7 @@ import tools
 import text_image
 import ascii_art
 import ascii_image_editor
+import color_matrix
 # from IPython.testing.iptest import have
 # from scipy.sparse.linalg.eigen.arpack.tests.test_arpack import CheckingLinearOperator
 
@@ -33,8 +34,8 @@ import ascii_image_editor
 input_image_filename = 'bitcoin.png'
 data_text_filename = 'half_paper.txt'#satoshi whitepaper in a txt file
 
-original_ascii_art_filename = 'ascii_' + input_image_filename.split('.')[0] + '.txt'
-edited_ascii_art_filename = 'EDITED_' + original_ascii_art_filename
+# original_ascii_art_filename = 'ascii_' + input_image_filename.split('.')[0] + '.txt'#need???????????????????????????????????????
+# edited_ascii_art_filename = 'EDITED_' + original_ascii_art_filename#need?????????????????????????????????????????????
 # text_image_filename = 'EDITED_data_dash.txt'      #picture of bitcoin icon
 
 final_image_filename = 'TEST_OUTPUT.png'
@@ -57,52 +58,55 @@ desired_dimension_ratio = 1/1
 #find correct image dimensions by adjusting desired ratio for the difference between the length of a char and the height of a line
 true_dimension_ratio = desired_dimension_ratio * const_HxW_ratio
 
+#need????????????????????????????????????????????????????????????????????????????????????????????????
+# colors = {'backround_1':  (255,255,255),#white
+#           'backround_2':  (0,0,0),#black
+#           'default_text': (255,255,255),#white
+#           'highlight':    {}
+#                                                                         }
+# char_types = {'remove'      : [':>{"'],
+#               'blend'       : [''],
+#               'color'       : "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'.",#['-', '=','+','*','#','%', '@'],
+#               'whitespace'  : [' ']}
 
-colors = {'backround_1':  (255,255,255),#white
-          'backround_2':  (0,0,0),#black
-          'default_text': (255,255,255),#white
-          'highlight':    {}
-                                                                        }
-char_types = {'remove'      : [':>{"'],
-              'blend'       : [''],
-              'color'       : "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'.",#['-', '=','+','*','#','%', '@'],
-              'whitespace'  : [' ']}
+# tinker = False
 
-tinker = False
+# #returns lines to make an ascii art txt file of image AND returns dict matching chars to colors (awful func, should be split up)
+# print('Converting image to ascii art lines and getting color equivalents...')
+# original_ascii_lines, color_chars = ascii_art.convert_image_to_color_equiv_ascii_art(input_image_filename, cols, scale)
 
-#returns lines to make an ascii art txt file of image AND returns dict matching chars to colors (awful func, should be split up)
-print('Converting image to ascii art lines and getting color equivalents...')
-original_ascii_lines, color_chars = ascii_art.convert_image_to_color_equiv_ascii_art(input_image_filename, cols, scale)
-
-#writes ascii lines to a txt file, not needed for function but helpful for tinkering
-print('Writing original ascii lines to txt file...')
-tools.write_text_file(original_ascii_art_filename, original_ascii_lines)
+# #writes ascii lines to a txt file, not needed for function but helpful for tinkering
+# print('Writing original ascii lines to txt file...')
+# tools.write_text_file(original_ascii_art_filename, original_ascii_lines)
 
 #still need some way to get char_types from color_chars, probably through UI!!!!!!!!!!!!!!
 
-#add the necessary chars with their color equivalents to highlight chars
-for c_char, color in color_chars.items():
-    if c_char in char_types['color']:
-        colors['highlight'][c_char] = color
-print('colors:', colors)#````````````````````````````````````````````````````````````````````````
-print('num_highlight_cords:', len(colors['highlight']))#```````````````````````````````````````````````````````````
+# #add the necessary chars with their color equivalents to highlight chars
+# for c_char, color in color_chars.items():
+#     if c_char in char_types['color']:
+#         colors['highlight'][c_char] = color
+# print('colors:', colors)#````````````````````````````````````````````````````````````````````````
+# print('num_highlight_cords:', len(colors['highlight']))#```````````````````````````````````````````````````````````
 
 
-if tinker == False:    
-    #creates new txt file by editing the original ascii txt image, removes to slim sharpen image, blends to set up for multi-color final image
-    print('Creating edited ascii art lines...')
-    edited_ascii_lines = ascii_image_editor.edit_ascii_lines(original_ascii_lines, char_types)  
+# if tinker == False:    
+# #creates new txt file by editing the original ascii txt image, removes to slim sharpen image, blends to set up for multi-color final image
+# print('Creating edited ascii art lines...')
+# edited_ascii_lines = ascii_image_editor.edit_ascii_lines(original_ascii_lines, char_types)  
 
-    #also need something to check for and correct incomplete editing - blend chars left behind, ect..., probably just UI
+#also need something to check for and correct incomplete editing - blend chars left behind, ect..., probably just UI
 
-    #writes ascii lines to a txt file, not needed for function but helpful for tinkering
-    print('Writing edited ascii lines to txt file...')
-    tools.write_text_file(edited_ascii_art_filename, edited_ascii_lines ) 
+# #writes ascii lines to a txt file, not needed for function but helpful for tinkering
+# print('Writing edited ascii lines to txt file...')
+# tools.write_text_file(edited_ascii_art_filename, edited_ascii_lines ) 
     
-else:
-    #read in tinkered with edited txt file
-    print('Reading in data text file...') 
-    edited_ascii_lines = tools.read_text_file(edited_ascii_art_filename)
+# else:
+# #read in tinkered with edited txt file
+# print('Reading in data text file...') 
+# edited_ascii_lines = tools.read_text_file(edited_ascii_art_filename)
+
+#gets matrix of colors from image
+img_color_matrix = color_matrix.get_color_tile_matrix(input_image_fileName, cols, scale, true)
 
 #read in the text that will be colored to show a picture
 print('Reading in data text file...') 
@@ -114,7 +118,6 @@ num_chars = len(data_str)
 word_list = data_str.split(' ')
 
 
-
 #turn true_dimension_ratio into max number of lines and max chars per line
 ideal_dimentions = tools.calc_ideal_dimentions(true_dimension_ratio, num_chars)
 
@@ -122,11 +125,11 @@ ideal_dimentions = tools.calc_ideal_dimentions(true_dimension_ratio, num_chars)
 lines = tools.make_correct_lines(ideal_dimentions['num_lines'], ideal_dimentions['line_length'], word_list)
 # print("number of lines:", len(lines))
 
-#look at text image lines to get cords of chars to be highlighted in final image
-highlight_cords = tools.get_highlight_cords(edited_ascii_lines)
-# print(highlight_cords)#``````````````````````````````````````````````````````````````````````````````````````````````````````````````
-# for h_char, cords in highlight_cords.items():#`````````````````````````````````````````````````````````````````````````````````````
-#     print(h_char)#````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+# #look at text image lines to get cords of chars to be highlighted in final image
+# highlight_cords = tools.get_highlight_cords(edited_ascii_lines)
+# # print(highlight_cords)#``````````````````````````````````````````````````````````````````````````````````````````````````````````````
+# # for h_char, cords in highlight_cords.items():#`````````````````````````````````````````````````````````````````````````````````````
+# #     print(h_char)#````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
 
 #adjust the highlight cords to compensate for the difference between the width of a char and the height of a line
 adjusted_highlight_cords = tools.adjust_highlight_cords(highlight_cords, image_resize_ratio)
