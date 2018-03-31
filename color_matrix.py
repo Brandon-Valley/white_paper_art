b36cfe279efa56de8b0dabbba8f77e5ff14af373
Brandon Valley <bavvh8@mst.edu>
2018-03-31 02:57:54 -0500

    cleaned up a bit, also about to delete color_reader

--- color_matrix.py
+++ color_matrix.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-import color_reader
+
+import tools
 
 
 def get_color_tile_matrix(fileName, cols, scale, moreLevels):    
@@ -22,7 +23,6 @@ def get_color_tile_matrix(fileName, cols, scale, moreLevels):
     # compute tile height based on aspect ratio and scale #used to be h
     tile_h = tile_w / scale
     
-    print('in color_matrix, tile_h: %s  tile_w: %s' %(tile_h, tile_w))#```````````````````````````````````````````````````````````````````
 
     # compute number of rows
     rows = int(H / tile_h)
@@ -38,21 +38,11 @@ def get_color_tile_matrix(fileName, cols, scale, moreLevels):
     # generate list of dimensions
     for j in range(rows):
         y_pos = j * tile_h
-        print('y_pos: ', y_pos)#``````````````````````````````````````````````````````````````````````````````
         
-#         tile_color_matrix.append([]) #dont need!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         tcm_row = []
-        
-        y1 = int(j*tile_h)
-        y2 = int((j+1)*tile_h)
- 
-        # correct last tile
-        if j == rows-1:
-            y2 = H
  
         for i in range(cols):
             x_pos = i * tile_w
-            print('  x_pos: ', x_pos)#```````````````````````````````````````````````````````````````````````````````
              
             tile_colors = {}
             
@@ -70,46 +60,9 @@ def get_color_tile_matrix(fileName, cols, scale, moreLevels):
                     y += 1
               
                 x += 1
-            
-            
-#             for x in range( int( tile_w ) ):
-#                 print('    x: ', x)#````````````````````````````````````````````````````````````````````````````
-#                 for y in range( int( tile_h ) ):
-#                     print('      y: ', y)#`````````````````````````````````````````````````````````````````````
-#                     r, g, b = color_image.getpixel(( x_pos + x , y_pos + y ))
-#                     #check if this color has been seen before, if not, add to list, if so, increase count
-#                     if (r, g, b) in tile_colors:
-#                         tile_colors[(r, g, b)] += 1
-#                     else:
-#                         tile_colors[(r, g, b)] = 1
                          
-            tcm_row.append(color_reader.high_key(tile_colors))
+            tcm_row.append(tools.high_key(tile_colors))
         tile_color_matrix.append( tcm_row )
 
-
- 
-#             # crop image to tile
-#             x1 = int(i*tile_w)
-#             x2 = int((i+1)*tile_w)
-#     
-#             # correct last tile
-#             if i == cols-1:
-#                 x2 = W
-#                 
-#             #crop color tile then convert to jpg
-#             color_tile_img_BMP = color_image.crop((x1, y1, x2, y2))
-#                 
-#             #.crop wants to make color_tile into .BMP which I dont feel like dealing with so save as jpg to work with it
-#             temp_filename = 'color_tile'  + '.jpg' #+ str(count_)
-#             color_tile_img_BMP.save(temp_filename)
-#             color_tile_img = Image.open(temp_filename)
-#             rgb_color_tile_img = color_tile_img.convert('RGB')#not sure if needed
-# #                 rgb_color_tile_img.show()#`````````````````````````````````````````````````````````````
-#                 
-#             #get most common color in the color tile 
-#             tile_W, tile_H = rgb_color_tile_img.size[0], rgb_color_tile_img.size[1]
-#             tile_color = color_reader.most_common_color(rgb_color_tile_img, tile_H, tile_W)
-#     
-#             tile_color_matrix[j].append( tile_color )
 
     return tile_color_matrix
--- color_reader.py
+++ color_reader.py
@@ -1,74 +1,74 @@
-#might be useful later, saves image with only N most-frequent colors:  https://stackoverflow.com/questions/3241929/python-find-dominant-most-common-color-in-an-image
-
-from PIL import Image
-
-
-
-#return key whose value is highest
-def high_key(d):
-    high_val = 0
-    high_key = 0
-    for key, val in d.items():
-        if val > high_val:
-            high_key = key
-            high_val = val
-    return high_key
-
-
-#returns single most common color in image
-def most_common_color(rgb_image, height, width):
-#     print('in color_reader, height: %s  width: %s' %(height, width))#`````````````````````````````````````````````````````````````````````
-    image_colors = {}
-    for x in range(width):
-        for y in range(height):
-            r, g, b = rgb_image.getpixel((x, y))
-            #check if this color has been seen before, if not, add to list, if so, increase count
-            if (r, g, b) in image_colors:
-                image_colors[(r, g, b)] += 1
-            else:
-                image_colors[(r, g, b)] = 1
-
-    #return the color with the highest count
-    return high_key(image_colors)
-
-# def most_common_color_odd_file(color_tile_img_BMP):
-#     #.crop wants to make color_tile into .BMP which I dont feel like dealing with so save as jpg to work with it
-#     temp_filename = 'color_tile.jpg'
-#     color_tile_img_BMP.save(temp_filename)
-#     color_tile_img = Image.open(temp_filename)
-#     rgb_color_tile_img = color_tile_img.convert('RGB')#not sure if needed
-# #                 rgb_color_tile_img.show()#`````````````````````````````````````````````````````````````
-#     
-#     tile_W, tile_H = rgb_color_tile_img.size[0], rgb_color_tile_img.size[1]
-#     color_equiv = color_reader.most_common_color(rgb_color_tile_img, tile_H, tile_W)
-# #                 print ('equiv color:', color_equiv)#```````````````````````````````````````````````````````````````
-#     
-#     #done with jpg
-#     rgb_color_tile_img.close()
-#     color_tile_img.close()
-#     os.remove(temp_filename)
-#     
-#     return color_equiv
-
-
-
-#example main
-def main():
-    input_image_filename = 'color_tile1.jpg'
-    im = Image.open(input_image_filename)
-    rgb_im = im.convert('RGB')#not sure if needed
-     
-    # store dimensions
-    W, H = rgb_im.size[0], rgb_im.size[1]
-    print("input image dims: %d x %d" % (W, H))
-     
-    print(most_common_color(rgb_im, H, W))
-
-
-# # call main
-if __name__ == '__main__':
-    main()
-
-
+# #might be useful later, saves image with only N most-frequent colors:  https://stackoverflow.com/questions/3241929/python-find-dominant-most-common-color-in-an-image
+# 
+# from PIL import Image
+# 
+# 
+# 
+# #return key whose value is highest
+# def high_key(d):
+#     high_val = 0
+#     high_key = 0
+#     for key, val in d.items():
+#         if val > high_val:
+#             high_key = key
+#             high_val = val
+#     return high_key
+# 
+# 
+# #returns single most common color in image
+# def most_common_color(rgb_image, height, width):
+# #     print('in color_reader, height: %s  width: %s' %(height, width))#`````````````````````````````````````````````````````````````````````
+#     image_colors = {}
+#     for x in range(width):
+#         for y in range(height):
+#             r, g, b = rgb_image.getpixel((x, y))
+#             #check if this color has been seen before, if not, add to list, if so, increase count
+#             if (r, g, b) in image_colors:
+#                 image_colors[(r, g, b)] += 1
+#             else:
+#                 image_colors[(r, g, b)] = 1
+# 
+#     #return the color with the highest count
+#     return high_key(image_colors)
+# 
+# # def most_common_color_odd_file(color_tile_img_BMP):
+# #     #.crop wants to make color_tile into .BMP which I dont feel like dealing with so save as jpg to work with it
+# #     temp_filename = 'color_tile.jpg'
+# #     color_tile_img_BMP.save(temp_filename)
+# #     color_tile_img = Image.open(temp_filename)
+# #     rgb_color_tile_img = color_tile_img.convert('RGB')#not sure if needed
+# # #                 rgb_color_tile_img.show()#`````````````````````````````````````````````````````````````
+# #     
+# #     tile_W, tile_H = rgb_color_tile_img.size[0], rgb_color_tile_img.size[1]
+# #     color_equiv = color_reader.most_common_color(rgb_color_tile_img, tile_H, tile_W)
+# # #                 print ('equiv color:', color_equiv)#```````````````````````````````````````````````````````````````
+# #     
+# #     #done with jpg
+# #     rgb_color_tile_img.close()
+# #     color_tile_img.close()
+# #     os.remove(temp_filename)
+# #     
+# #     return color_equiv
+# 
+# 
+# 
+# #example main
+# def main():
+#     input_image_filename = 'color_tile1.jpg'
+#     im = Image.open(input_image_filename)
+#     rgb_im = im.convert('RGB')#not sure if needed
+#      
+#     # store dimensions
+#     W, H = rgb_im.size[0], rgb_im.size[1]
+#     print("input image dims: %d x %d" % (W, H))
+#      
+#     print(most_common_color(rgb_im, H, W))
+# 
+# 
+# # # call main
+# if __name__ == '__main__':
+#     main()
+# 
+# 
 
 
