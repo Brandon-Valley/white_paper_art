4b18f5841aeb3d286f5b964cb94d61ed4fa6f738
Brandon Valley <bavvh8@mst.edu>
2018-04-01 04:18:11 -0500

    make get_dimentions

--- offset.py
+++ offset.py
@@ -8,6 +8,8 @@ def offset_color_cords(original_color_cords, img_pos):
 #inclomlete, will probably need line dimentions to do this right!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
 def calc_offset(color_cords, img_pos_dict):
     dim_dict = get_dimentions(color_cords)
+    
+    print('in offsed, dims: ', dim_dict)#``````````````````````````````````````````````````````````
     if offset_type_str == 'centered':
         bad_default_offset_dict = {'x_offset': 10,
                                    'y_offset': 10}
@@ -15,9 +17,34 @@ def calc_offset(color_cords, img_pos_dict):
     else:
         raise Exception('ERROR  Unknown offset type: ', offset_type_str)
     
-
+#get height and width, mins are 0 because of trim in color_cords so only need to find max
 def get_dimentions(c_cords):
-    print('in offset, c_cords: ', c_cords)#`````````````````````````````````````````````````````````````````````````
-    print('in offset, fancy print c_cords: VVVVV')
+    #find x and y max
+    x_max = None
+    y_max = None
+    
     for color, cord_list in c_cords.items():
-        print("%s : %s" %(cord_list, color))
+        for cord in cord_list:
+            x = cord[1]
+            y = cord[0]
+            if x_max == None and y_max == None:
+                x_max = x
+                y_max = y
+            else:
+                if x_max < x:
+                    x_max = x
+                if y_max < y:
+                    y_max = y
+                    
+    dims = {'width': x_max,
+            'height': y_max}
+    return dims
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+
