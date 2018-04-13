44b3447399939227358dccbce1f23f2e2fa00812
Brandon Valley <bavvh8@mst.edu>
2018-04-12 20:23:41 -0500

    nothing broken yet

--- GUI.py
+++ GUI.py
@@ -5,13 +5,20 @@ import build_image
 import GUI_utils
 
 
-def build_img_btn_clk():
-    print(font_drop_down.get())
+def build_img_btn_clk(image_args):
+#     print(font_drop_down.get())
+    print('in GUI, image_args: ', image_args)
+    build_image.build_img_test(image_args)
 
 
 def show_gui():
     
-    img_args = {'text_file_path':           None,
+    #default arguments
+    input_text_file_path = GUI_utils.get_defalt_text_file_path()
+    input_img_file_path= GUI_utils.get_defalt_image_file_path()
+    
+    #initialize img_args
+    img_args = {'input_text_file_path':           None,
                 'image_file_path':          None,
                 'max_font_size':            None,
                 'font_name':                None,
@@ -20,21 +27,31 @@ def show_gui():
                 'image_position_cords':     None,
                 'output_image_file_path':   None}
 
-
     window = Tk()
     window.title("Text Image Maker")
     window.geometry('350x200')
     
-    #text file path text box
-    text_file_path_lbl = Label(window, text="Text File Input: ")
-    text_file_path_lbl.grid(column=0, row=0)
-    text_file_path_text_box = Entry(window,width=10)
-    text_file_path_text_box.insert(END, 'default/path')
-    def clicked():
-        text_file_path = 'test/file/path'
-
-    text_file_path_btn = Button(window, text="Directory", command=clicked)
     
+    #text file path text box
+    input_text_file_path_lbl = Label(window, text="Text File Input: ")
+    input_text_file_path_text_box = Entry(window,width=10)
+    input_text_file_path_text_box.insert(END, input_text_file_path)
+    
+    def input_text_file_path_clk():
+        input_text_file_path = input_text_file_path_text_box.get()
+
+    input_text_file_path_btn = Button(window, text="Directory", command = input_text_file_path_clk)
+    
+    
+    #image file path text box
+    input_img_file_path_lbl = Label(window, text="Text File Input: ")
+    input_img_file_path_text_box = Entry(window,width=10)
+    input_img_file_path_text_box.insert(END, input_img_file_path)
+    
+    def input_img_file_path_clk():
+        input_img_file_path = input_img_file_path_text_box.get()
+
+    input_img_file_path_btn = Button(window, text="Directory", command = input_img_file_path_clk)
     
     
     
@@ -49,18 +66,29 @@ def show_gui():
         
         
     #build image button
-    build_img_btn = Button(window, text="Build Image", command = build_img_btn_clk)
+    build_img_btn = Button(window, text="Build Image", command = build_img_btn_clk(img_args))
     
-    text_file_path_text_box.grid(column=1, row=0)
-    text_file_path_btn.grid(column=2, row=0)
     
-    font_drop_down.grid(column=0, row=3)
     
-    build_img_btn.grid(column=3, row=3)
+    #input text file path
+    input_text_file_path_lbl        .grid(column=0, row=0)
+    input_text_file_path_text_box   .grid(column=1, row=0)
+    input_text_file_path_btn        .grid(column=2, row=0)
+    
+    #input image file path
+    input_img_file_path_lbl         .grid(column=0, row=1)
+    input_img_file_path_text_box    .grid(column=1, row=1)
+    input_img_file_path_btn         .grid(column=2, row=1)
+    
+    #font drop down
+    font_drop_down                  .grid(column=0, row=3)
+    
+    #build image button
+    build_img_btn                   .grid(column=2, row=4)
     
 
     
-    build_image.build_img_test(img_args)
+    
     window.mainloop()
     
     
--- GUI_utils.py
+++ GUI_utils.py
@@ -1,4 +1,11 @@
 
 
+def get_defalt_text_file_path():
+    return 'default/text/file/path'
+
+def get_defalt_image_file_path():
+    return 'default/image/file/path'
+
+
 def get_font_list():
     return['font1', 'font2', 'font3']
