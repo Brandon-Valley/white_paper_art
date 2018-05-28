d72daaf9b818adc1a3d159e6544d434e66d37856
Brandon Valley <bavvh8@mst.edu>
2018-05-28 18:01:17 -0500

    made double click selection work for lbox

--- Advanced_Tab.py
+++ Advanced_Tab.py
@@ -59,7 +59,7 @@ class Advanced_Tab(Tab.Tab):
         
     def input_background_color______widget_setup(self):
 
-        def update_color_display_with_selected_color(): #event = None #dont remove until tested to make sure you dont need!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        def update_color_display_with_selected_color(event = None): #event = None #dont remove until tested to make sure you dont need!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             #update tuple text box
             self.input_bgnd_clr_tup_tb.configure(state = 'normal')
             self.input_bgnd_clr_tup_tb.delete(0, END)
