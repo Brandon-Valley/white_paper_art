4d031836403416468e32071348a2009817a64f7e
Brandon Valley <bavvh8@mst.edu>
2019-01-15 10:49:54 -0600

    load unkown char csv now works by using append

--- load_unknown_chars_csv.py
+++ load_unknown_chars_csv.py
@@ -15,13 +15,22 @@ def char_already_corrected(unknown_char_unicode, corrected_chars_dl):
     return False 
     
 
-
-def add_corrected_chars(corrected_chars_dl, unknown_chars_dl):
+def find_new_corrected_chars(corrected_chars_dl, unknown_chars_dl):
+    new_corrected_chars_dl = []
+    
     for unknown_char_d in unknown_chars_dl:
         if char_already_corrected(unknown_char_d['unknown_char_unicode'], corrected_chars_dl) == False:
             unknown_char_d.pop('#_occurrences')
-            corrected_chars_dl.append(unknown_char_d)
-    return corrected_chars_dl
+            new_corrected_chars_dl.append(unknown_char_d)
+    return new_corrected_chars_dl
+
+
+# def add_corrected_chars(corrected_chars_dl, unknown_chars_dl):
+#     for unknown_char_d in unknown_chars_dl:
+#         if char_already_corrected(unknown_char_d['unknown_char_unicode'], corrected_chars_dl) == False:
+#             unknown_char_d.pop('#_occurrences')
+#             corrected_chars_dl.append(unknown_char_d)
+#     return corrected_chars_dl
     
 
 
@@ -45,14 +54,14 @@ def load_unknown_chars_csv(unknown_chars_csv_path, corrected_chars_csv_path):
     else:
         og_corrected_chars_dl = []
         
-    final_corrected_chars_dl = add_corrected_chars(og_corrected_chars_dl, unknown_chars_dl)
-    print(len(final_corrected_chars_dl))#`````````````````````````````````````````````````````````````````````````````````````````````````````
-
-    for d in final_corrected_chars_dl:
-        print(d.keys())#`    ```````````````````````````````````````````````````````````````````````````````````````````````
+#     final_corrected_chars_dl = add_corrected_chars(og_corrected_chars_dl, unknown_chars_dl) #```````````````````````````````````````````
+    new_corrected_chars_dl = find_new_corrected_chars(og_corrected_chars_dl, unknown_chars_dl)
+#     print(new_corrected_chars_dl)#```````````````````````````````````````````````````````````````````````````````````````````````````
     
     header_order_list = ['correct_char', 'unknown_char_unicode', 'example']
-    logger.logList(final_corrected_chars_dl, corrected_chars_csv_path, WANT_BACKUP, header_order_list, 'overwrite')
+    
+    if new_corrected_chars_dl != []:
+        logger.logList(new_corrected_chars_dl, corrected_chars_csv_path, WANT_BACKUP, header_order_list, 'append')
 
 
 
